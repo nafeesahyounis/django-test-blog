85559c727220f562afabc0ed428edb3e0c6f255b
nafeesahyounis <nafeesah.youniss@gmail.com>
2020-12-28 20:32:07 +0000

    added models category, tags and post to blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, blank=True)
+    
+    def __str__(self):
+        return self.name
+
+
+class Tags(models.Model):
+    name = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, blank=True)
+    
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=500, blank=False)
+    slug = models.SlugField(max_length=250, null=True, blank=True)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    tags = models.ForeignKey(Tags, null=True, blank=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    content = models.TextField()
+
+
+
+   
+
