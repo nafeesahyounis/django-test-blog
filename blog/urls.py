bf3db2ffdd166ba1f4008372932ae6864043a49f
nafeesahyounis <nafeesah.youniss@gmail.com>
2020-12-29 20:26:16 +0000

    created post detail page and added code in views to make it accessible by slug

--- blog/urls.py
+++ blog/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('<slug>', views.post_detail, name='post_detail'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post, Category
 
 # Create your views here.
+
 
 def home(request):
     """A view to return the home page"""
@@ -20,5 +21,17 @@ def home(request):
         'current_categories': categories
         }
     return render(request, 'blog/index.html', context)
+
+
+def post_detail(request, slug):
+    """ A view to show individual post details """
+
+    posts = Post.objects.get(slug=slug)
+
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'blog/post_detail.html', context)
 
     
